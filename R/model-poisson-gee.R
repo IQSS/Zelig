25bc75e91bf5ecc2a3e879ed5063a0192c98a34d
cchoirat <cchoirat@gmail.com>
2014-03-18 15:40:49 -0400

    Added Poisson GEE model and demo, updated NAMESPACE

---
+++ R/model-poisson-gee.R
@@ -0,0 +1,53 @@
+zpoissongee <- setRefClass("Zelig-poisson-gee", contains="Zelig")
+
+zpoissongee$methods(
+  initialize = function() {
+    callSuper()
+    .self$model <- "poissongee"
+    .self$year <- 2011
+    .self$category <- "continuous"
+    .self$authors <- "Patrick Lam"
+    .self$text = "General Estimating Equation for Poisson Regression"
+    .self$fn <- quote(gee::gee)
+  }
+)
+
+zpoissongee$methods(
+  zelig = function(formula, id, ..., R = NULL, corstr = "independence", data) {
+    if (corstr == "fixed" && is.null(R))
+      stop("R must be defined")
+    # if id is a valid column-name in data, then we just need to extract the
+    # column and re-order the data.frame and cluster information
+    if (is.character(id) && length(id) == 1 && id %in% colnames(data)) {
+      id <- data[, id]
+      data <- data[order(id), ]
+      id <- sort(id)
+    }
+    .self$zelig.call <- match.call(expand.dots = TRUE)
+    callSuper(formula=formula, data=data, ...)
+    .self$model.call$family  = quote(poisson())
+    .self$model.call$id <- id
+    .self$model.call$R <- R
+    .self$model.call$corstr <- corstr
+    .self$zelig.out <- eval(.self$model.call)
+  }
+)
+
+zpoissongee$methods(
+  param = function(num) {
+    .self$simparam <- mvrnorm(n=num, mu=.self$zelig.out$coefficients,
+                              Sigma=.self$zelig.out$naive.variance)
+  }
+)
+
+zpoissongee$methods(
+  qi = function(x) {
+    coef <- .self$simparam
+    eta <- coef %*% t(x)
+    theta <- matrix(poisson()$linkinv(eta), nrow = nrow(coef))
+    # To mimick Zelig 4 (incorrect behavior of "inverse" function):
+    # theta <- matrix(1 / eta, nrow = nrow(coef))
+    ev <- theta
+    return(list("Expected Values: E(Y|X)"  = ev))
+  }
+)
---
+++ test/demo-poisson-gee.R
@@ -0,0 +1,33 @@
+source("utils.R")
+source("model-zelig.R")
+source("model-glm.R")
+source("model-poisson-gee.R")
+
+# Zelig 5 code:
+set.seed(42)
+z5 <- zpoissongee$new()
+z5$zelig(num ~ target + coop, id = "cluster",
+         data = sorted.sanction, corstr = "exchangeable")
+z5
+z5$zelig.call
+z5$model.call
+z5$param(10)
+z5$setx()
+set.seed(42)
+z5$sim(num=10)
+z5$summarize()
+z5$cite()
+
+# Zelig 4 code:
+library(Zelig)
+data(sanction)
+sanction$cluster <- c(rep(c(1:15), 5), rep(c(16), 3))
+sorted.sanction <- sanction[order(sanction$cluster), ]
+z.out <- zelig(num ~ target + coop, model = "poisson.gee", id = "cluster",
+               data = sorted.sanction, robust = FALSE, corstr = "exchangeable")
+summary(z.out)
+x.out <- setx(z.out)
+set.seed(42)
+s.out <- sim(z.out, x.out, num=10)
+summary(s.out)
+
