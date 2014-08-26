8882d413bcdd22c349554ccdd153f9ca3a8be3c7
Merce Crosas <mcrosas@hmdc.harvard.edu>
2014-08-26 13:52:08 -0400

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -118,7 +118,7 @@ html_theme_options = {
     #     ("Link", "http://example.com", True),
     # ],
     'navbar_links': [
-        ("Data Science at IQSS", "http://datascience.iq.harvard.edu/zelig", True),
+        ("Roadmap", "http://54.191.56.66/milestones/milestone-roadmap/Zelig5/", True),
     ],
     
     # Global TOC depth for "site" navbar tab. (Default: 1)
