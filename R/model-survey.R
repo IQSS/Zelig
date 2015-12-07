8bc0bc912fe4ceeea3ea48824af8262edd363908
tercer <jhonaker@iq.harvard.edu>
2015-12-07 16:15:40 -0500

    add replication design

--- R/model-survey.R
+++ R/model-survey.R
@@ -13,12 +13,19 @@ zsurvey$methods(
 )
 
 zsurvey$methods(
-  zelig = function(formula, data, ids=~1, probs=NULL, strata=NULL, fpc=NULL, nest=FALSE, check.strata=!nest, ... , weights = NULL, by = NULL) {
+  zelig = function(formula, data, ids = ~1, probs = NULL, strata = NULL, fpc = NULL, nest = FALSE, check.strata = !nest, 
+                   repweights = NULL, type = NULL, combined.weights = FALSE, rho = NULL, bootstrap.average = NULL, scale = NULL,
+                   rscales = NULL, fpctype = "fraction", ... , weights = NULL, by = NULL) {
     .self$zelig.call <- match.call(expand.dots = TRUE)
-    design <- survey::svydesign(data= data, ids=~1, probs=probs, strata=strata, fpc=fpc, nest=nest, check.strata=check.strata)
+    if(is.null(repweights)){
+      design <- survey::svydesign(data=data, ids=~1, probs=probs, strata=strata, fpc=fpc, nest=nest, check.strata=check.strata, weights=weights)
+    }else{
+      design <- survey::svrepdesign(data=data, repweights=repweights, type=type, weights=weights, combined.weights=combined.weights, rho=rho, 
+                                    bootstrap.average=bootstrap.average, scale=scale, rscales=rscales, fpctype=fpctype, fpc=fpc)
+    }
     .self$model.call <- as.call(list(.self$fn, formula=.self$zelig.call$formula,  design=design))  # fn will be set again by super, but initialized here for clarity
     .self$model.call$family <- call(.self$family, .self$link)
-    callSuper(formula = formula, data = data, ..., weights = weights, by = by)
+    callSuper(formula = formula, data = data, ..., by = by)
   }
 )
 
