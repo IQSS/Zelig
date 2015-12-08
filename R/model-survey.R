e5493a0c86c2f749529c7435001267b8aad361c9
tercer <jhonaker@iq.harvard.edu>
2015-12-07 20:06:52 -0500

    checking and conversion of var.names to formula

--- R/model-survey.R
+++ R/model-survey.R
@@ -17,6 +17,43 @@ zsurvey$methods(
                    repweights = NULL, type = NULL, combined.weights = FALSE, rho = NULL, bootstrap.average = NULL, scale = NULL,
                    rscales = NULL, fpctype = "fraction", ... , weights = NULL, by = NULL) {
     .self$zelig.call <- match.call(expand.dots = TRUE)
+
+    recastString2Formula <- function(a){
+      if(is.character(a)){
+        a <- as.formula(paste("~",a))
+      }
+      return(a)
+    }
+
+    checkLogical <- function(a, name=""){
+      if(!("logical" %in% class(a))){
+        cat(paste("Warning: argument ",name," is a logical and should be set to TRUE for FALSE.", sep=""))
+        return(FALSE)
+      }else{
+        return(TRUE)
+      }
+
+    }
+
+    ## Check arguments:
+
+    ## Zelig generally accepts formula names of variables present in dataset, 
+    ##   but survey package looks for formula expressions or data frames, 
+    ##   so make conversion of any character arguments.
+    ids<-recastString2Formula(ids)
+    probs<-recastString2Formula(probs)
+    weights<-recastString2Formula(weights)
+    strata<-recastString2Formula(strata)
+    fpc<-recastString2Formula(fpc)   
+    checkforerror <- checkLogical(nest, "nest")
+    checkforerror <- checkLogical(check.strata, "check.strata")
+    repweights<-recastString2Formula(repweights)
+    # type should be a string 
+    checkforerror <- checkLogical(combined.weights, "combined.weights")
+    # rho is shrinkage factor
+    # scale is scaling constant
+    # rscales is scaling constant
+
     if(is.null(repweights)){
       design <- survey::svydesign(data=data, ids=~1, probs=probs, strata=strata, fpc=fpc, nest=nest, check.strata=check.strata, weights=weights)
     }else{
