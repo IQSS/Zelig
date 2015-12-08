17b3c24bc95473e4bd33ff8f07a4d20ff274b30b
tercer <jhonaker@iq.harvard.edu>
2015-12-08 16:43:22 -0500

    removed qi, as defined correctly by inheritance from binchoice

--- R/model-binchoice-survey.R
+++ R/model-binchoice-survey.R
@@ -17,23 +17,7 @@ zbinchoicesurvey$methods(
     callSuper()
     .self$family <- "binomial"
     .self$category <- "continuous"
-    #.self$fn <- quote(geepack::geeglm)
     # JSON from parent
   }
 )
 
-zbinchoicesurvey$methods(
-  qi = function(simparam, mm) {
-    .self$linkinv <- eval(call(.self$family, .self$link))$linkinv
-    coeff <- simparam
-    eta <- simparam %*% t(mm)
-    eta <- Filter(function (y) !is.na(y), eta)
-    theta <- matrix(.self$linkinv(eta), nrow = nrow(coeff))
-    ev <- matrix(.self$linkinv(eta), ncol = ncol(theta))
-    pv <- matrix(nrow = nrow(ev), ncol = ncol(ev))
-    for (j in 1:ncol(ev))
-      pv[, j] <- rbinom(length(ev[, j]), 1, prob = ev[, j])
-    levels(pv) <- c(0, 1)
-    return(list(ev = ev, pv = pv))
-  }
-)
