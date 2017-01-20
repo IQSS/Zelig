5b21f3043e00dd4be8cbe078d4f0e6a65daf6c0c
Christopher Gandrud <christopher.gandrud@gmail.com>
2017-01-20 16:14:29 -0500

    continuous covar REQUIRE TEST

--- tests/testthat/test-ls.R
+++ tests/testthat/test-ls.R
@@ -1,3 +1,23 @@
-z <- zls$new()
-test <- z$mcunit(plot=FALSE)
-expect_true(test)
+# Monte Carlo test -------------------------------------------------------------
+
+test_that('REQUIRE TEST ls monte carlo test', {
+    z <- zls$new()
+    test <- z$mcunit(plot = FALSE)
+    expect_true(test)
+})
+
+# Documentation examples -------------------------------------------------------
+
+test_that('REQUIRE TEST ls continuous covar -- quickstart (Zelig 5 syntax)', {
+    z5 <- zls$new()
+    z5$zelig(Fertility ~ Education, data = swiss)
+  
+    # extract education coefficient parameter estimate and compare to reference
+    expect_equivalent(round(as.numeric(z5$getcoef()[[1]][2]), 7), -0.8623503)
+})
+
+# gim method tests -------------------------------------------------------------
+
+#test_that('REQUIRE TESTls gim method', {
+    #z5$gim()
+#})
