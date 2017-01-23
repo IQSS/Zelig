2340a044a9b5eaaaa2119974b8b93717fb155b0f
Christopher Gandrud <cgandrud@hmdcadministrators-iMac.local>
2017-01-23 11:06:27 -0500

    # This is a combination of 2 commits. # This is the 1st commit message:
    begin NEWS
    
    # This is the commit message #2:
    
    begin sim num test

--- tests/testthat/test-zelig.R
+++ tests/testthat/test-zelig.R
@@ -1,8 +1,20 @@
-# Integration tests for the Zelig estimate, set, sim, plot workflow
+# Integration tests for the Zelig estimate, set, sim, plot workflow ------------
 
+
+# FAIL TEST sim workflow -------------------------------------------------------
 test_that('FAIL TEST sim method warning if missing Zelig object', {
     z5 <- zls$new()
     z5$zelig(Fertility ~ Education, data = swiss)
     
     expect_warning(z5$sim())
+})
+
+# REQUIRE TEST sim custom num --------------------------------------------------
+testthat('REQUIRE TEST sim custom num in sim', {
+    z5 <- zls$new()
+    z5$zelig(Fertility ~ Education, data = swiss)
+    z5$setx(Education = 15)
+    z5$sim()
+  
+    z5$sim.out
 })
