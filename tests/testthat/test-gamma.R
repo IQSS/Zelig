7eb13bdbf96e5dfb5de02e6857219eaa8c17370f
tercer <jameshonaker2@gmail.com>
2017-02-01 01:41:19 -0500

    adjust simulation parameters for MC unit tests

--- tests/testthat/test-gamma.R
+++ tests/testthat/test-gamma.R
@@ -2,6 +2,6 @@
 
 test_that('REQUIRE TEST gamma Monte Carlo', {
     z <- zgamma$new()
-    test <- z$mcunit(b0=1, b1=-0.6, alpha=3, minx=0, maxx=1, nsim=2000, ci=.99, plot = FALSE)
-    expect_true(test)
+    test.gamma <- z$mcunit(b0=1, b1=-0.6, alpha=3, minx=0, maxx=1, nsim=2000, ci=.99, plot = FALSE)
+    expect_true(test.gamma)
 })
--- tests/testthat/test-normalsurvey.R
+++ tests/testthat/test-normalsurvey.R
@@ -2,6 +2,6 @@
 
 test_that('REQUIRE TEST normalsurvey Monte Carlo', {
     z <- znormalsurvey$new()
-    test <- z$mcunit(plot = FALSE)
-    expect_true(test)
+    test.normalsurvey <- z$mcunit(plot = FALSE)
+    expect_true(test.normalsurvey)
 })
