75f80587e7990822539d52f9db03b46a4b8f13c2
christophergandrud <christopher.gandrud@gmail.com>
2017-02-17 08:25:57 -0500

    minor index page formatting

--- vignettes/about.R
+++
@@ -1,8 +0,0 @@
-## ----setup, include=FALSE------------------------------------------------
-knitr::opts_knit$set(
-stop_on_error = 2L
-)
-
-## ----echo=FALSE, background='ffffff', comment=''-------------------------
-citation('Zelig')
-
