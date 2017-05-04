44c7e9918aa6fd79eaa53fcc26ac22ac09506cbe
christophergandrud <christopher.gandrud@gmail.com>
2017-05-03 20:11:12 -0700

    resolve negbin issue

--- tests/testthat/test-negbin.R
+++ tests/testthat/test-negbin.R
@@ -1,3 +1,9 @@
-z <- znegbin$new()
-test.negbin <- z$mcunit(plot=FALSE)
-expect_true(test.negbin)
+
+# REQUIRE TEST Monte Carlo test negbin ---------------------------------------------
+
+test_that('REQUIRE TEST negbin Monte Carlo', {
+    set.seed(123)
+    z <- znegbin$new()
+    test.negbin <- z$mcunit(plot=FALSE)
+    expect_true(test.negbin)
+})
