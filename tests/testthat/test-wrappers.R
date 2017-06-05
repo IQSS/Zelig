29cbcad533d178f4afffee9bd6dd78bc0051952f
christophergandrud <christopher.gandrud@gmail.com>
2017-06-05 14:04:53 -0400

    :minus: moved ZeligEI failures out of core Zelig tests

--- tests/testthat/test-wrappers.R
+++ tests/testthat/test-wrappers.R
@@ -83,18 +83,3 @@ test_that('REQUIRE TEST ATT wrapper', {
              get_qi(qi = "ATT", xvalue = "TE")
     expect_equal(length(my.att), length(z.att))
 })
-
-# FAIL TEST setx and sim should fail with ZeligEI ------------------------------
-test_that('FAIL TEST setx and sim should fail with ZeligEI', {
-    library("eiPack", quietly = TRUE)
-    data(senc)
-    z.out <- zeirxc$new()
-    z.out$zelig(cbind(dem, rep, non) ~ cbind(black, white, natam),
-                N = "total", data = senc)
-
-    expect_error(z.out$setx(), 'Function is not relevant for ZeligEI objects.')
-    expect_error(setx(z.out), 'Function is not relevant for ZeligEI objects.')
-
-    expect_error(z.out$sim(), 'Function is not relevant for ZeligEI objects.')
-    expect_error(sim(z.out), 'Function is not relevant for ZeligEI objects.')
-})
