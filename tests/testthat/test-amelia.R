362f69fa94e91b005b6e3b53e9db3fa40a22afbe
Ista Zahn <istazahn@gmail.com>
2017-06-23 12:46:51 -0400

    test for multiple imputation support in to_zelig

--- tests/testthat/test-amelia.R
+++ tests/testthat/test-amelia.R
@@ -21,3 +21,17 @@ test_that('REQUIRE TEST for Amelia integration, log-transformation', {
     z.outl <- zelig(gdp_pc ~ log(trade) + civlib, model = "ls", data = a.out)
     expect_false(coef(z.out)[[1]][2] == coef(z.outl)[[1]][2])
 })
+
+test_that('REQUIRE TEST for mutliple imputation support in to_zelig', {
+    data(africa, package = "Amelia")
+    a.out <- amelia(x = africa, cs = "country", ts = "year", logs = "gdp_pc")
+    mod.out <- with(imputationList(a.out$imputations), lm(gdp_pc ~ trade + civlib))
+    z.out <- zelig(gdp_pc ~ trade + civlib, model = "ls", data = a.out)
+    set.seed(481)
+    mod.sim <- zelig_qi_to_df(sim(setx(mod.out)))
+    set.seed(481)
+    z.sim <- zelig_qi_to_df(sim(setx(z.out)))
+    expect_equal(mod.sim, z.sim)
+})
+
+    
